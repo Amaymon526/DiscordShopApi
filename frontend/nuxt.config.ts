8e0a7db91908e935ea23890ccac8d2f2ff4a9c84
jeff <jeffrey.nelwek@varias.at>
2024-11-08 11:51:00 +0100

    first impl of auth - tiri

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  modules: ['@pinia/nuxt'],
+
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true, https: true },
+  devtools: { enabled: true },
+
+  devServer: {
+    https: true,
+  },
 
   app: {
-    pageTransition: { name: 'page', mode: 'slide-left' }
+    pageTransition: { name: 'slide-left', mode: "in-out" }
   },
 })
---
+++ frontend/pages/auth/callback.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+
+const route = useRoute();
+
+onMounted(() => {
+  handleDiscordRedirect()
+})
+
+function handleDiscordRedirect() {
+  const code = route.params.code;
+
+  if (code) {
+    fetch("http://localhost:8080/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({code}),
+    })
+        .then(response => response.json())
+        .then(data => {
+          console.log("User data:", data);
+        })
+        .catch(error => console.error("Error:", error));
+  }
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
