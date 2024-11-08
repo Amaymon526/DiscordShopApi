8c9e008528da3983814d3b79a8ad6fda43f498e5
jeff <jeffrey.nelwek@varias.at>
2024-11-08 11:51:18 +0100

    missing parts added

--- frontend/layout/default.vue
+++ frontend/layout/default.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-
+<div class="container">
+  <slot/>
+</div>
 </template>
 
 <style scoped>
-
+.container {
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
