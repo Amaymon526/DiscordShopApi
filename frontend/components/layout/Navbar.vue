9768480968ea6b6eee73b95082d4aa2b4619e83b
Tiri <tiri0007@yahoo.com>
2024-11-09 01:47:34 +0100

    first impl of layout components

---
+++ frontend/components/layout/Navbar.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav>
+    <div class="logo-container">
+      <p>Discord Porn Api</p>
+    </div>
+    <div class="links-container">
+      <ul id="navList">
+        <li>
+          <NuxtLink class="" activeClass="active" to="/">Home</NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+nav{
+
+  background: var(--background-second);
+  box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  /* Desktop View */
+  @media screen and (max-width: 1024px){
+    width: 250px;
+  }
+
+  /* Mobile View */
+  @media screen and (min-width: 1024px){
+    width: 100%;
+  }
+
+}
+</style>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,22 +1,14 @@
 <script setup lang="ts">
 
+import Navbar from "~/components/layout/Navbar.vue";
 </script>
 
 <template>
 <div class="container">
-  <div>
-    <p>This is the default layout</p>
-    <ul>
-      <li>
-        <NuxtLink to="/">Home</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/auth/callback">callback</NuxtLink>
-      </li>
-    </ul>
-  </div>
   <div class="content">
+    <Navbar/>
     <slot/>
+    <Footer/>
   </div>
 </div>
 </template>
